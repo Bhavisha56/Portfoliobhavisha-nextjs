a71bf05e7df5af7c04c3cf55717308c3d09bcfbc
bhavisha <bhavishakumar9600@gmail.com>
2025-09-14 23:22:51 +0530

    created the center part

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,26 @@
 'use client'
+import Center from "../components/center";
 const Home=()=>{
     return(
         <>
        <div className="h-screen bg-[url('/bgblack.jpg')] bg-cover bg-center">
-        <div className="text-white h-20 w-full flex p-10 justify-between text-3xl ">
+        <div className="text-white h-20 w-full flex p-10 justify-between text-3xl cursor-pointer">
             <div className="">
-               <h1 >BHAVISHA PORTFOLIO</h1>
+               <h1 className="text-xl">BHAVISHA PORTFOLIO</h1>
             </div>
             <div >
                 <ul className="flex gap-20">
-                    <li>About</li>
-                    <li>Project</li>
-                    <li>Contact</li>
+                    <li className="hover:text-lime-400 active:translate-y-0.5 transition-transform ">About</li>
+                    <li className="hover:text-lime-400 active:translate-y-0.5 transition-transform ">Project</li>
+                    <li className="hover:text-lime-400 active:translate-y-0.5 transition-transform ">Contact</li>
                 </ul>
             </div>
+        </div>
+        <div>
+            <Center/>
+        </div>
+        <div>
+            
         </div>
       </div>
         </>
---
+++ components/center.tsx
@@ -0,0 +1,15 @@
+'use client'
+const center=()=>{
+    return(
+        <>
+        <div className="centerbox  w-full h-96 mt-20 flex justify-center items-center ">
+          <div className="text-center cursor-pointer">
+              <h1 className="text-7xl">Welcome To My PortFolio</h1>
+              <h2 className="text-4xl ">I Am A Mern Stack Developer</h2>
+              <span className="text-2xl text-lime-400">Exploring More In Tech World...</span>
+          </div>
+        </div>
+        </>
+    )
+}
+export default center;
