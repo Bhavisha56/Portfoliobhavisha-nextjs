a7be25d4691e0a8b219fce465ef938a952701a38
bhavisha <bhavishakumar9600@gmail.com>
2025-09-18 21:32:46 +0530

    added the about page and image to be done later

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { useEffect, useRef } from "react";
@@ -17,7 +18,6 @@ const Home = () => {
     if (!container) return;
 
     const sections = container.querySelectorAll(".panel");
-
     gsap.to(sections, {
       xPercent: -100 * (sections.length - 1),
       ease: "none",
@@ -31,25 +31,32 @@ const Home = () => {
     });
   }, []);
 
-
   return (
     <div className="min-h-screen bg-zinc-900 text-white relative ">
-     
       <div className="absolute top-0 left-0 w-full z-50 bg-transparent">
         <Navbar />
       </div>
 
-  
-      <div 
-      ref={containerRef} className="flex w-[600vw] h-screen overflow-x-hidden overflow-y-hidden scrollbar-hide">
-      
+      <div
+        ref={containerRef}
+        className="flex w-[600vw] h-screen overflow-x-hidden overflow-y-hidden scrollbar-hide"
+      >
         <section className="panel w-screen h-screen flex flex-col items-center justify-center bg-[url('/bgblack.jpg')] bg-cover bg-center">
           <Center />
         </section>
 
         {/* AboutSection */}
-        <section className="panel w-screen h-screen flex items-center justify-center bg-gradient-to-r from-indigo-600 to-purple-800">
-          <h1 className="text-6xl font-bold">👋 About</h1>
+        <section className="panel bg-[url('/bgback.jpg')] bg-cover bg-center w-screen h-screen flex items-center justify-center ">
+          <div className="text-xl text-center text-lime-400 w-96 relative  right-52 h-56 font-bold">
+            <p className="aboutlines">I am a MERN Stack Developer and a B.Tech student in Electronics and
+            Communication Engineering at NSUT (2021–2025). I have hands-on
+            experience in building full-stack applications using React, Node.js,
+            Express, and MongoDB. I enjoy solving problems, designing scalable
+            applications, and exploring the intersection of software.</p>
+          </div>
+          <div className="bg-[url('/bgback.jpg')] bg-cover bg-center  right-5 w-56 h-56 rounded-full border-4 border-lime-400 relative left-52">
+             
+          </div>
         </section>
 
         {/* SkillsSection */}
